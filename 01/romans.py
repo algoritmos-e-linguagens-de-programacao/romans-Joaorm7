8d44ccbd25ae26cb1a1b7047a461ff7928f92abe
João Gabriel Romanin Seti <168040841+Joaorm7@users.noreply.github.com>
2024-06-18 22:05:49 -0300

    Update romans.py.123
    correção

--- 01/romans.py
+++ 01/romans.py
@@ -1,22 +1,20 @@
 def int_to_roman(num):
-
     num2 = num // 1000
     num3 = (num % 1000) // 100
     num4 = (num % 100) // 10
     num5 = num % 10
 
-    milhar = ["","M","MM","MMM"]
-    centena = ["","C","CC","CCC","CD","D","DC","DCC", "DCCC", "CM"]
-    dezena = ["","X","XX","XXX","XL","L","LX","LXX","LXXX", "XC"]
-    unidade = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+    milhar = ["", "M", "MM", "MMM"]
+    centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    dezena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    unidade = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
     traducao = milhar[num2] + centena[num3] + dezena[num4] + unidade[num5]
 
     return traducao
 
 def romano_para_inteiro(romano):
-
-valores = {
+    valores = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -26,7 +24,6 @@ valores = {
         'M': 1000
     }
 
-   
     total = 0
     valor_anterior = 0
 
@@ -41,3 +38,4 @@ valores = {
         valor_anterior = valor_atual
 
     return total
+
